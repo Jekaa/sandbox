bf05eeeb97d0f563bf4da1e86b94daaeb04de2e4
GordienkoEE <gordienko.e.evg@sberbank.ru>
2021-11-21 22:25:18 +0300

    init spring boot and dependencies

---
+++ src/test/java/ru/sandbox/SandboxApplicationTests.java
@@ -0,0 +1,15 @@
+package ru.sandbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+@TestPropertySource(properties = "management.metrics.export.wavefront.enabled=false")
+@SpringBootTest
+class SandboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
