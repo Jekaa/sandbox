2e7c87391a00c20b6e9dc85d804a1775f128117c
GordienkoEE <gordienko.e.evg@sberbank.ru>
2021-12-18 22:39:18 +0300

    change pom.xml + logger configuration

--- src/main/java/ru/sandbox/SandboxApplication.java
+++ src/main/java/ru/sandbox/SandboxApplication.java
@@ -2,8 +2,16 @@ package ru.sandbox;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
+import ru.sandbox.kafka.consumer.KafkaConsumerConfig;
+import ru.sandbox.kafka.consumer.KafkaConsumerProperties;
 
 @SpringBootApplication
+@PropertySource("classpath:application.properties")
+@EnableConfigurationProperties({KafkaConsumerProperties.class})
+@Import({KafkaConsumerConfig.class})
 public class SandboxApplication {
 
 	public static void main(String[] args) {
